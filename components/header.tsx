acbcab0a8fcf54223c5e3ce78c169fbbad729afc
Avoisavo <lvera296@gmail.com>
2026-01-18 14:48:33 +0700

    lol

--- components/header.tsx
+++ components/header.tsx
@@ -10,11 +10,9 @@ export function Header() {
             <div className="container mx-auto flex h-16 items-center justify-between px-4">
                 <div className="flex items-center gap-6">
                     <Link href="/" className="flex items-center gap-2">
-                        <img
-                            src="/phocastlogo.png"
-                            alt="Phocast Logo"
-                            className="h-8 w-auto"
-                        />
+                        <span className="text-xl font-bold text-black dark:text-white">
+                            Six Seven
+                        </span>
                     </Link>
                 </div>
 
